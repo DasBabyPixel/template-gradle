1cc7d8d49fda371d811aeb8224f15be5478d3f4e
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-09-26 16:17:17 -0400

    remove gradle plugin portal as plugin source

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         maven("https://reposilite.dasbabypixel.de/dasbabypixel") {
             name = "DasBabyPixel"
             credentials(PasswordCredentials::class)
